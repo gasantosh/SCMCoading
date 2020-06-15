a2876b9cfb36d3d9ab996aaed92947de4172906c
Santosh <gargsank@hotmail.com>
2020-06-15 23:10:44 +0530

    Adding Cart class and changed Item Class

---
+++ ScmCoading/Cart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScmCoading
+{
+    public class Cart
+    {
+        private List<Item> cartItems;
+        public Cart()
+        {
+            this.cartItems = new List<Item>();
+        }
+
+        public void AddItemToCart(string name, int quantity)
+        { 
+            this.cartItems.Add(new Item(name, quantity));
+        }
+
+        public IEnumerable<Item> GetCartItems()
+        {
+            return this.cartItems;
+        }
+    }
+}
--- ScmCoading/Item.cs
+++ ScmCoading/Item.cs
@@ -8,6 +8,12 @@ namespace ScmCoading
 {
     public class Item
     {
+        public Item(string name, int quantity)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+        }
+
         public Item(string name, int price, int quantity)
         {
             this.Name = name;
@@ -17,7 +23,7 @@ namespace ScmCoading
 
         public string Name { get; private set; }
 
-        public int Price { get; private set; }
+        public int? Price { get; private set; }
 
         public int Quantity { get; private set; }
     }
