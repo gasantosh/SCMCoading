80b0b24851fb681a27c8fba619ec05e626ef6c77
Santosh <gargsank@hotmail.com>
2020-06-15 22:38:24 +0530

    Adding Offer File

---
+++ ScmCoading/Offer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScmCoading
+{
+    public class Offer
+    {
+        private List<string> offerItesms;
+        public Offer()
+        {
+            this.offerItesms = new List<string>();
+        }
+
+        public List<string> OfferItems { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+    }
+}
