d7a7de09831f7f56192ed737116c83258847f857
Santosh <gargsank@hotmail.com>
2020-06-15 23:00:36 +0530

    Adding Item Class

---
+++ ScmCoading/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScmCoading
+{
+    public class Item
+    {
+        public Item(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; private set; }
+
+        public int Price { get; private set; }
+    }
+}
